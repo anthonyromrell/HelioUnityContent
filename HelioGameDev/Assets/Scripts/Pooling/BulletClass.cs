b7b2dc448b97ac499819ce7caf3d4ac3ddd9c252
Anthony Romrell <anthonyromrell@mac.com>
2017-02-06 12:51:55 -0700

    Upgraded Pooling

---
+++ HelioGameDev/Assets/Scripts/Pooling/BulletClass.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System;
+
+public class BulletClass : MonoBehaviour
+{
+    //Sends the bullet gameObject to any adding function.
+    public static Action<GameObject> SendBullet;
+    //PoolBullet is added to it.
+
+    void Start()
+    {
+        SendBullet(gameObject);//Calls the action and passes the bullet gameObject.
+        gameObject.SetActive(false);//turns the bullet off at the start.
+    }
+    //OnEnable() is a Unity function that runs when an object is activated
+    void OnEnable()
+    {
+        transform.position = new Vector3();// resets the bullet position
+        GetComponent<Rigidbody>().AddForce(200, 0, 0);//gets the rigid body and adds force to x
+
+        if (SendBullet != null)//checks if SendBullet doesn't contain functions 
+        {
+            SendBullet(gameObject);//Calls the action and passes the bullet gameObject.
+            Invoke("Disable", 10);//Delays the Disable function by 10 seconds
+        }
+    }
+    void Disable()
+    {
+        gameObject.SetActive(false);//turns off the bullet gameObject.
+        GetComponent<Rigidbody>().Sleep();//stops the Rigidbody forces.
+    }
+}
---
+++ HelioGameDev/Assets/Scripts/Pooling/PoolBullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class PoolBullet : MonoBehaviour {
+
+	bool canFire = true;//if true runs the while loop in the coroutine.
+	public List<GameObject> bullets;//a list of bullet GameObjects.
+
+	//runs at the start of the game.
+	void Start () { 
+		//adds the AddBulletsToList function to
+		//the BulletClass.SendBullet action.
+		BulletClass.SendBullet += AddBulletsToList;
+		StartCoroutine(RecycleBullets());//starts the RecycleBullets coroutine
+	}
+    private void AddBulletsToList(GameObject obj)
+    {
+        bullets.Add(obj);//adds the bullet GameObjects to the bullets list
+    }
+
+	IEnumerator RecycleBullets () {
+		yield return new WaitForEndOfFrame();//waits to run at the end of a frame
+		while (canFire)//checks if canFire is true.
+		{
+			bullets[0].SetActive(true);//sets the first bullet in the list to active.
+			bullets.RemoveAt(0);//removes the list item in the postion of the number.
+			yield return new WaitForSeconds(1);//holds for second to loop back
+		}
+	}
+}
