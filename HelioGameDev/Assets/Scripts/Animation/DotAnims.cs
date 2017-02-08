653cd5aca916be87e4cf51a29d4041fc88c48f21
Anthony Romrell <anthony@artisandream.com>
2017-02-08 00:05:13 -0700

    Animation

---
+++ HelioGameDev/Assets/Scripts/Animation/DotAnims.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class DotAnims : MonoBehaviour {
+
+	private Animator anims;
+
+	// Use this for initialization
+	void Start () {
+		anims = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void OnMouseUp () {
+		anims.SetBool("MouseUp", true);
+		anims.SetBool("Drag", false);
+	}
+
+	void OnMouseDown()
+	{
+		anims.SetBool("Drag", true);
+	}
+
+	public void EndAnim () {
+		anims.SetBool("MouseUp", false);
+	}
+
+	public void StartDrag () {
+		anims.SetBool("Drag", false);
+	}
+}
