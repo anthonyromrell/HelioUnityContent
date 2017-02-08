c6d558a091c8ab6b5b9ae94dd193c8e83e6864f5
Anthony Romrell <anthony@artisandream.com>
2017-02-08 00:05:03 -0700

    Added Animation State Mechines

---
+++ HelioGameDev/Assets/Scripts/InstanceVsPooling/BulletClass.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System;
+
+public class BulletClass : MonoBehaviour
+{
+    public static Action<GameObject> SendBullet;
+
+    void Start()
+    {
+        SendBullet(gameObject);
+		gameObject.SetActive(false);
+    }
+    void OnEnable()
+    {
+		transform.position = new Vector3();
+		GetComponent<Rigidbody>().AddForce(200,0,0);
+        if (SendBullet != null)
+        {
+            SendBullet(gameObject);
+            Invoke("Disable", 10);
+		}
+    }
+    void Disable()
+    {
+        gameObject.SetActive(false);
+		GetComponent<Rigidbody>().Sleep();
+    }
+}
