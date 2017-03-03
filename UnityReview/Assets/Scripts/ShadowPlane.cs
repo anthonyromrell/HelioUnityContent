fca18ad2ed093e084c1715e7090ef6f5154c4337
Anthony Romrell <anthony@artisandream.com>
2017-03-03 01:06:31 -0700

    Modeling in Maya

---
+++ UnityReview/Assets/Scripts/ShadowPlane.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ShadowPlane : MonoBehaviour {
+
+	public Transform player;
+	Vector3	reverseRotate;
+	Quaternion revRot;
+
+	// Update is called once per frame
+	void Update () {
+		//revRot.y = player.rotation.y*-1;
+		//transform.rotation = revRot;
+
+		reverseRotate.y = player.rotation.eulerAngles.y*-1;
+		transform.rotation = Quaternion.Euler(reverseRotate); 
+	}
+}
